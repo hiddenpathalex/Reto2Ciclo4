faff31c0b1e04162aa8da75ea05dfc07dd4d26e3
Jhonatan Alexander <91511883+hiddenpathalex@users.noreply.github.com>
2021-12-03 20:51:11 -0500

    Add files via upload

---
+++ src/main/java/mintic/misiontic/ciclo4/reto2/reto2/Reto2Application.java
@@ -0,0 +1,13 @@
+package mintic.misiontic.ciclo4.reto2.reto2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto2Application.class, args);
+	}
+
+}
